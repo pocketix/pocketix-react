68df1988cec22ab1c94ee12de318c757f665dbb0
maara3 <marekchvatik@gmail.com>
2026-04-15 11:36:43 +0200

    Changed URL allowlist

--- packages/pocketix-editor/src/index.tsx
+++ packages/pocketix-editor/src/index.tsx
@@ -7,10 +7,7 @@ import posthog from 'posthog-js';
 posthog.init('phc_Rnee6Qq5vgXvJ5NCh2ls9RIFvWgpO83bjuUb53yvss2', {
     api_host: 'https://posthog.pocketix.org',
     person_profiles: 'always',
-    autocapture: {
-            url_allowlist: ['http://localhost:3200/'],
-            url_ignorelist: [],
-        }
+    autocapture: true
 });
 
 export {PocketixEditor};
